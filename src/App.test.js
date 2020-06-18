1204251e94a62e65bb948c4fd43f34a6ff69f707
Jan Czibula <jan.czibula@gmail.com>
2020-06-18 15:41:41 +0200

    update readme

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/QuizApp/i);
   expect(linkElement).toBeInTheDocument();
 });
